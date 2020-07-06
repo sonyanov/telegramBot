b59b461a1567291e585b2f555c700695aa73f11d
sonyanov <60970319+sonyanov@users.noreply.github.com>
2020-07-06 20:16:18 +0300

    fix

---
+++ src/main/java/exception/GroupNoInputInfoException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class GroupNoInputInfoException extends Exception {
+
+    public GroupNoInputInfoException(){
+        super();
+    }
+}
--- src/main/java/exception/groupNoInputInfoException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class groupNoInputInfoException extends Exception {
-
-    public groupNoInputInfoException(){
-        super();
-    }
-}
